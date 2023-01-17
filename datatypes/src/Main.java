9406a1fa2e354ad5789806282da77220ee5238a6
kayaunal <77548290+kayaunal@users.noreply.github.com>
2023-01-17 23:54:38 +0300

    .

---
+++ datatypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 12.5;
+        sayi = -129;
+        char karakter = 'A';
+        System.out.println(sayi);
+
+        boolean dogruMu=false;
+        if (dogruMu==true)
+        {System.out.println("Doğru");
+        }else {
+            System.out.println("Yanlış");
+        }
+    }
+}
