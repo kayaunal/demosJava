162322aae8a37e95d3eac6bfdf8ddff87da0861c
kayaunal <77548290+kayaunal@users.noreply.github.com>
2023-01-28 21:01:33 +0300

    .

---
+++ arraysDemo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String ogrenci = "Engin";
+        String ogrenci2 = "Derin";
+        String Ogrenci3 = "Salih";
+
+    }
+}
---
+++ conditionals/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi=214;
+        if (sayi<20){
+            System.out.println("Sayı 20'den küçüktür.");
+        }else{
+            System.out.println("Sayı 20'den küçük değildir.");
+        }
+    }
+}
