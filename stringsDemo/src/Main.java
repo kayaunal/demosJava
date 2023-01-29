7ed3bf3f34b07e46357a7957fd948462c175b09a
kayaunal <77548290+kayaunal@users.noreply.github.com>
2023-01-29 23:23:19 +0300

    .

--- stringsDemo/src/Main.java
+++ stringsDemo/src/Main.java
@@ -12,5 +12,23 @@ public class Main {
         String mesaj2 = mesaj.concat(" Yaşasın!");
         System.out.println(mesaj2);
 
+        System.out.println(mesaj.startsWith("B"));
+        System.out.println(mesaj.endsWith("."));
+
+        char[] karakterler = new char[5];
+        mesaj.getChars(0,5, karakterler,0);
+        System.out.println(karakterler);
+        System.out.println(mesaj.indexOf('a'));
+        System.out.println(mesaj.lastIndexOf('a'));
+
+        String yeniMesaj = mesaj.replace(' ', '-');
+                System.out.println(yeniMesaj);
+
+                System.out.println(mesaj.substring(2,4));
+
+                for (String kelime:mesaj.split(" ")){
+                    System.out.println(kelime);
+                }
+
     }
 }
