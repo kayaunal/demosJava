b10b2a70441ad38a593fcfc4fccab923ad641067
kayaunal <kayaunal@hotmail.com>
2023-02-28 22:22:24 +0300

    28.02.23

--- workingWithFiles/src/Main.java
+++ workingWithFiles/src/Main.java
@@ -1,9 +1,13 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        getFileInfo();
+        //createFile();
+        //getFileInfo();
+        readFile();
+        writeFile();
+        readFile();
     }
     public static void createFile(){
         File file = new File("C:\\demosJava\\files\\students.txt");
@@ -25,6 +29,30 @@ public class Main {
             System.out.println("Dosya yazılabilir mi: "+ file.canWrite());
             System.out.println("Dosya okunabilir mi: "+ file.canRead());
             System.out.println("Dosya boyutu (byte): "+ file.length());
+        }
+    }
+    public static void readFile(){
+        File file = new File("C:\\demosJava\\files\\students.txt");
+        try {
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()){
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void writeFile(){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\demosJava\\files\\students.txt", true));
+            writer.newLine();
+            writer.write("Ahmet");
+            System.out.println("Dosyaya yazıldı");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
