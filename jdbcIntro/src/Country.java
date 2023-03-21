2526ebfbf51a8199a3ae82361fbf1fb9fbcf63e0
kayaunal <kayaunal@hotmail.com>
2023-03-21 17:11:37 +0300

    21.03.23

---
+++ jdbcIntro/src/Country.java
@@ -0,0 +1,14 @@
+public class Country {
+    private String code;
+    private String name;
+    private String continent;
+    private String region;
+
+    public Country(String code, String name, String continent, String region){
+        this.code=code;
+        this.name= this.name;
+        this.continent= this.continent;
+        this.region= this.region;
+    }
+
+}
--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -1,5 +1,92 @@
-public class Main {
-    public static void main(String[] args) {
+import java.sql.*;
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+        try {
+            connection = helper.getConnection();
+            String sql = "delete from city where ID=?";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, 4083);
+            int result = statement.executeUpdate();
+            System.out.println("Kayıt silindi");
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            statement.close();
+            connection.close();
+        }
+    }
+
+    public static void selectDemo() throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+        Statement statement = null;
+        ResultSet resultSet;
+        try {
+            connection = helper.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select Code, Name, Continent, Region from country");
+            ArrayList<Country> countries = new ArrayList<>();
+            while (resultSet.next()) {
+                countries.add(new Country(
+                        resultSet.getString("code"),
+                        resultSet.getString("name"),
+                        resultSet.getString("continent"),
+                        resultSet.getString("region")));
+            }
+            System.out.println(countries.size());
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+            System.out.println(exception.getMessage());
+        } finally {
+            statement.close();
+            connection.close();
+        }
+    }
+    public static void  insertData() throws SQLException{
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+        try {
+            connection = helper.getConnection();
+            String sql = "insert into city (Name, CountryCode, District, Population) values(?,?,?,?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,"EklenenŞehir");
+            statement.setString(2,"TUR");
+            statement.setString(3,"District");
+            statement.setInt(4,75000);
+            int result = statement.executeUpdate();
+            System.out.println("Kayıt eklendi");
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            statement.close();
+            connection.close();
+        }
+    }
+    public static void updateData() throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+        try {
+            connection = helper.getConnection();
+            String sql = "update city set District = 'EklenenŞehir', Population=125000 where ID=?";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, 4083);
+            int result = statement.executeUpdate();
+            System.out.println("Kayıt güncellendi");
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            statement.close();
+            connection.close();
+        }
     }
 }
